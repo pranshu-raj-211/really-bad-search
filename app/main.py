1e067bc943f9d900d9eb737c96530acb3781a862
pranshu-raj-211 <pranshuraj65536@gmail.com>
2025-02-13 10:49:00 +0530

    add logger, exception handling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import asyncio
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from models.video_model import PaginatedResponse
 from db.db_utils import get_paginated_videos
+from utils.log_config import logger
 from services.pull_videos import YoutubeFetchDaemon
 
 app = FastAPI()
@@ -13,18 +14,23 @@ fetch_daemon = YoutubeFetchDaemon()
 async def lifespan():
     # start daemon on app startup
     video_fetch_task = asyncio.create_task(fetch_daemon.start_fetching())
+    logger.info('Started pulling videos from youtube.')
     yield
 
     # when app is stopped, kill daemon
     fetch_daemon.stop_fetching()
+    logger.info('Stopping pulling videos from api.')
     await video_fetch_task
 
 @app.get("/")
 async def root() -> dict[str, str]:
+    logger.info('Server up')
     return {"message": "Up and running"}
 
 @app.get("/videos", response_model=PaginatedResponse)
 async def get_videos(page: int = Query(1, ge=1), page_size: int = Query(10, ge=1, le=50)):
-    return await get_paginated_videos(page, page_size)
-
-# TODO: run the fetch daemon on startup
+    try:
+        return await get_paginated_videos(page, page_size)
+    except Exception as e:
+        logger.error("Error occured while serving videos", extra={'exception':e})
+        raise HTTPException(status_code=500, detail='Something went wrong while serving videos.')
