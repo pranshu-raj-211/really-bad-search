38e838de80fbc36fcb18a83a0da9686fd2173a4a
Pranshu Raj <113211320+pranshu-raj-211@users.noreply.github.com>
2025-02-13 10:55:30 +0530

    add list api url

--- app/constants.py
+++ app/constants.py
@@ -1,4 +1,4 @@
-SEARCH_TERM = 'cricket'
-LIST_API_URL=''
+SEARCH_TERM = "cricket"
+LIST_API_URL="https://www.googleapis.com/youtube/v3/search"
 MAX_FETCH_RESULTS = 50
 FETCH_INTERVAL = 600    # seconds, 10 minutes
